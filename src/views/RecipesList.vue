81a721e400b5fb5dcb1efa6658589b984661e0de
JULO01 <90795588+JULO01@users.noreply.github.com>
2022-03-28 19:13:09 +0200

    feat: added a image in the empty-state v-card in recipes-list

--- src/views/RecipesList.vue
+++ src/views/RecipesList.vue
@@ -13,18 +13,36 @@
       @closing="closeEditableRecipe()"
     />
 
-    <v-card v-if="ownRecipes.length == 0">
+    <v-card v-if="ownRecipes.length == 0" class="mt-4">
+      <v-card-content>
+        <div class="d-flex flex-column justify-space-between align-center">
+          <v-img
+            width="180"
+            src="https://firebasestorage.googleapis.com/v0/b/recipe-book-5e2f4.appspot.com/o/assets%2Fempty-state.png?alt=media&token=ae17a028-036b-4fe4-a131-920a69122fb3"
+          >
+          </v-img>
+        </div>
+      </v-card-content>
       <v-card-text>
-        <p class="text-h4 text--primary mt-8">
-          Seems like you don't have any recipes yet...
-        </p>
+        <div class="d-flex flex-column justify-space-between align-center">
+          <p class="text-h4 text--primary mt-8">
+            Seems like you don't have any recipes yet...
+          </p>
+        </div>
       </v-card-text>
-      <v-card-actions>
-        <v-btn to="/random-recipe" text color="teal accent-4"> Browse random recipes </v-btn>
+      <v-card-actions class="justify-center">
+        <v-btn to="/random-recipe" text color="primary">
+          Browse random recipes
+        </v-btn>
       </v-card-actions>
     </v-card>
 
-    <v-list class="mt-8" v-if="completeRecipeHidden && editableRecipeHidden && ownRecipes.length != 0">
+    <v-list
+      class="mt-8"
+      v-if="
+        completeRecipeHidden && editableRecipeHidden && ownRecipes.length != 0
+      "
+    >
       <div
         class="listItem"
         v-for="recipe in ownRecipes.slice().reverse()"
@@ -96,6 +114,6 @@ export default {
   },
   computed: {
     ...mapState(["ownRecipes", "completeRecipeHidden", "editableRecipeHidden"]),
-  },   
+  },
 };
 </script>
